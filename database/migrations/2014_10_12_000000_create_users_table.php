f03a24e14710bbe18d4d17288b3cc16e77e27cda
Alejandro Alamar <ale.jaam7@gmail.com>
2021-04-11 12:08:11 -0500

    Modificar tablas con migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,14 +22,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        Schema::create('courses', function (Blueprint $table) {
-            $table->id();
-            $table->string('name_course');
-            $table->string('author');
-            $table->string('calification');
-            $table->string('category');
-            $table->timestamps();
-        });
     }
 
     /**
